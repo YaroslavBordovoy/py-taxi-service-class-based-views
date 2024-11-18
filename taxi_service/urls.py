cb048e06dbbaba25a40f22d799df8772cac158a7
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-11-19 00:30:58 +0200

    Removed debug toolbar

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -22,5 +21,4 @@ from django.urls import path, include
 urlpatterns = ([
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + debug_toolbar_urls())
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
